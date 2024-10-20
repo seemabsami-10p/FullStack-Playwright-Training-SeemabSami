ef196cb0eb9548cf4b480ed01fba5cdee668a048
seemabsami-10p <143487849+seemabsami-10p@users.noreply.github.com>
2024-10-21 04:11:57 +0500

    Updates secondSessionAssignment.spec.js

--- tests/secondSessionAssignment.spec.js
+++ tests/secondSessionAssignment.spec.js
@@ -1,15 +1,20 @@
+import {URL,USERNAME, PASSWORD} from '../test-constants/constants.js';
 const { test, expect } = require('@playwright/test');
 
-test('Visit Website', async ({ page }) => {
+test('login', async ({ page }) => {
 
-    // Automation code written here
-    await page.goto('https://app.pur.jeenie.com/');
-    await page.locator("#input-29").click();
-    await page.locator("#input-29").fill("owner-enterprise-payg@example.com");
-    await page.locator("#input-33").fill("password");
-    expect(await page.getByRole('loginbtn',{id:'loginbtn'})).toBeEnabled;
-    await page.getByRole('button', { name: 'Login' });
-    //await page.waitForLoadState("https://app.pur.jeenie.com/account/call");
+    // Login on Jeenie Test Envoirnment
+    await page.goto(URL);
+    await page.locator('#input-0').click();
+    await page.locator('#input-0').fill(USERNAME);
+    await page.locator('#input-2').click();
+    await page.locator('#input-2').fill(PASSWORD);
+    expect(page.getByRole('Sign In',{name:'Sign In'})).toBeEnabled;
+    await page.getByText('Sign In').click();
+    expect(page.getByText("Hello Owner!").isVisible);
+    console.log ('User logged in successfully');
+    
+   
   
   });
 
